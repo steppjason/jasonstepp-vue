b8fa68b3929760e4e9563b2641f6ad75256c4759
Jason Stepp <hello@jasonstepp.me>
2022-01-09 06:16:11 -0700

    Updated social icons, added email

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <h1 class="header-name"><span>Jason</span> Stepp</h1>
-    <h2 class="header-subtitle">Developer</h2>
+    <h2 class="header-subtitle">Full Stack Developer</h2>
     <nav>
       <ul>
         <li><router-link to="/">Home</router-link></li>
@@ -9,6 +9,9 @@
         <li><router-link to="/about">About</router-link></li>
       </ul>
     </nav>
+    <div class="email">
+      <svg class="icon email-icon" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" width="512px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><polygon points="448,384 448,141.8 316.9,241.6 385,319 383,321 304.1,251.4 256,288 207.9,251.4 129,321 127,319 195,241.6    64,142 64,384  "/><polygon points="439.7,128 72,128 256,267.9  "/></g></svg>
+      <a href="mailto:hi@jasonstepp.me">hi@jasonstepp.me</a></div>
     <div class="socials">
       <ul>
         <li><a title="LinkedIn - Jason Stepp" href="https://www.linkedin.com/in/steppjason/" target="_blank"><svg class="icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path d="M19,3H5C3.895,3,3,3.895,3,5v14c0,1.105,0.895,2,2,2h14c1.105,0,2-0.895,2-2V5C21,3.895,20.105,3,19,3z M7.738,17L7.738,17 c-0.697,0-1.262-0.565-1.262-1.262v-4.477C6.477,10.565,7.042,10,7.738,10h0C8.435,10,9,10.565,9,11.262v4.477 C9,16.435,8.435,17,7.738,17z M7.694,8.717c-0.771,0-1.286-0.514-1.286-1.2s0.514-1.2,1.371-1.2c0.771,0,1.286,0.514,1.286,1.2 S8.551,8.717,7.694,8.717z M16.779,17L16.779,17c-0.674,0-1.221-0.547-1.221-1.221v-2.605c0-1.058-0.651-1.174-0.895-1.174 s-1.058,0.035-1.058,1.174v2.605c0,0.674-0.547,1.221-1.221,1.221h-0.081c-0.674,0-1.221-0.547-1.221-1.221v-4.517 c0-0.697,0.565-1.262,1.262-1.262h0c0.697,0,1.262,0.565,1.262,1.262c0,0,0.282-1.262,2.198-1.262C17.023,10,18,10.977,18,13.174 v2.605C18,16.453,17.453,17,16.779,17z"/></svg></a></li>
