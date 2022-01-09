2b2af43416d4702e0e4d5ed96e5de0136ba2c61f
Jason Stepp <hello@jasonstepp.me>
2022-01-09 08:39:36 -0700

    Mobile menu open and close functions

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,23 @@
 <template>
     <nav>
       <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/projects">Projects</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
+        <li><router-link v-on:click="closeMenu" to="/">Home</router-link></li>
+        <li><router-link v-on:click="closeMenu" to="/projects">Projects</router-link></li>
+        <li><router-link v-on:click="closeMenu" to="/about">About</router-link></li>
       </ul>
     </nav>
 </template>
+
+<script>
+
+export default {
+  name: "Nav",
+  components: {
+  },
+  methods:{
+      closeMenu(){
+          this.$parent.$refs.menu.classList.remove("open")
+      }
+  }
+}
+</script>
