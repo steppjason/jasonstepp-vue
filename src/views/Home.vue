f867b170fb79c6cb534ee2549d03cfb2064b53e2
Jason Stepp <hello@jasonstepp.me>
2022-01-10 13:35:02 -0700

    Home page content, tweaked styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="home content">
     <h3>Hi,</h3>
-    <p></p>
+    <p>I am a <span class="emphasize bold">developer</span></p>
+    <p>I like to <span class="bold">learn</span> , <span class="bold">create</span>,</p>
+    <p>and <span class="bold">build</span> things</p>
+    <ul>
+      <li>C#</li>
+      <li>PHP</li>
+      <li>HTML5</li>
+      <li>CSS3</li>
+      <li>JavaScript</li>
+      <li>MySQL</li>
+      <li>Git/GitHub</li>
+      <li>SASS</li>
+      <li>jQuery</li>
+      <li>REST</li>
+      <li>Unix</li>
+      <li>Apache</li>
+      <li>NGINX</li>
+      <li>WordPress</li>
+      <li>Unity</li>
+      <li>Photoshop</li>
+      <li>Illustrator</li>
+    </ul>
   </div>
 </template>
 
