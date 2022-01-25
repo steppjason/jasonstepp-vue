fc32c846fc39b6488f1b54886f34ba8d283fbc85
Jason Stepp <hello@jasonstepp.me>
2022-01-25 16:32:12 -0700

    About me page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,18 +2,14 @@
 
 	<div class="content">
 		<h3>About Me</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-		commodo erat non feugiat consequat. Suspendisse ac fermentum metus.
-		Aliquam erat volutpat. Proin bibendum aliquam elementum.</p>
-		<p>Nulla facilisi. Vivamus id pharetra sem. Aliquam felis quam, lacinia
-		at dapibus in, semper sit amet ligula. Integer vel velit et ligula
-		convallis congue a eu magna. Donec sed lorem urna. Vivamus blandit
-		vel elit nec bibendum. Pellentesque imperdiet sem ac bibendum
-		rutrum.</p>
-		<p>Mauris ac erat at ante bibendum consectetur. In tellus sem, congue
-		rhoncus lectus a, imperdiet tristique turpis. Pellentesque nec est
-		ac risus pretium accumsan sed vitae dolor. In et tortor sem. Aliquam
-		pellentesque neque sit amet orci fermentum imperdiet.</p>
+		<p>My name is Jason Stepp. I am a developer that likes to learn, create, and build things.</p>
+		<p>My interest in computers started as young as I can remember. Mostly fueled by my interest 
+		in video games. Which lead me into the world of programming. Something which I pursue both 
+		professionally as well as personally.</p>
+		<p>Since then I have been learning and exploring all areas that allowed me to create things 
+		with computers. Including video games, animation, 3D modeling, websites, and software. Naturally, 
+		I pursued those same interests through college and my career.</p>
+		<p>I love to learn new things and I am always interested in new technologies and skills.</p>
 	</div>
 
 </template>
